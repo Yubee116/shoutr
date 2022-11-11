d8f9039bf16405ed40219a923f24f1e78bd49288
Yubee116 <ubongnehemiah@gmail.com>
2022-11-11 13:22:20 +0100

    Added username/email sign_in support
    - modified sign_in form partial for text in general (vs email)
    - extended clearance sessions controller to support email/username sign_in
    - Added guest model to prevent failure if user does not exist
    - modified routes to use "local" sessions controller
    
    **cleaned users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < Clearance::UsersController
-    private
+  private
 
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
